b1ab10a5ac977020abef7232b325b1a96f4da29f
swarnachowdary08 <40885933+swarnachowdary08@users.noreply.github.com>
2018-07-07 09:22:47 +0530

    Create positve.py

---
+++ positve.py
@@ -0,0 +1,20 @@
+    #include<stdio.h>
+    #include<conio.h>
+    int main()
+    {
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+    printf("the no is positive");
+    }
+    else if(n<0)
+    {
+    printf("the number is negative");
+    }
+    else
+    {
+    printf("the number is equal to 0");
+    }
+    return o;
+    }
